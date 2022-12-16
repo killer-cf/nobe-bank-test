1baa95e566ba43ee351d54623e68a5c3d7cf5c31
killer-cf <costa.kilder@live.com>
2022-12-16 11:29:04 -0300

    client sign in

--- spec/factories/clients.rb
+++ spec/factories/clients.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :client do
-    
+    name { 'Kilder Costa Filho' }
+    cpf_number { '12345678901' }
+    email { 'costa.kilder@live.com' }
+    password { 'password' }
   end
 end
---
+++ spec/system/authentication/client_signs_in_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+describe 'client signs in' do
+  it 'success' do
+    create :client, name: 'Kilder Costa', password: 'password123', email: 'costa.kilder@gmail.com'
+
+    visit root_path
+    click_on 'Entrar'
+    fill_in 'E-mail', with: 'costa.kilder@gmail.com'
+    fill_in 'Senha', with: 'password123'
+    find('.actions').click_on 'Entrar'
+
+    expect(page).to have_current_path root_path
+    expect(page).to have_content('Kilder Costa')
+    expect(page).not_to have_link('Entrar')
+    expect(page).to have_content('Autenticação efetuada com sucesso.')
+  end
+
+  it 'with blank fields' do
+    create :client, name: 'Kilder Costa', password: 'password123', email: 'costa.kilder@gmail.com'
+
+    visit root_path
+    click_on 'Entrar'
+    fill_in 'E-mail', with: ''
+    fill_in 'Senha', with: ''
+    find('.actions').click_on 'Entrar'
+
+    expect(page).to have_content 'E-mail ou senha inválidos.'
+    expect(page).not_to have_content 'Autenticação efetuada com sucesso.'
+  end
+end
