38a80cd6a19a1ca07eda0d25efeabf2e9f736696
killer-cf <costa.kilder@live.com>
2022-12-19 09:44:45 -0300

    feat/client unit tests

--- spec/models/client_spec.rb
+++ spec/models/client_spec.rb
@@ -1,4 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Client, type: :model do
+  describe '#valid?' do
+    it { is_expected.to validate_presence_of(:name) }
+
+    it { is_expected.to validate_presence_of(:cpf_number) }
+
+    it { is_expected.to validate_presence_of(:email) }
+  end
 end
