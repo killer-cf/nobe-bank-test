c740d37ab8b560b10900c4c1362a5e8f9190990c
killer-cf <costa.kilder@live.com>
2022-12-20 09:01:45 -0300

    fix/filter shows statements another clients

--- app/controllers/account_statements_controller.rb
+++ app/controllers/account_statements_controller.rb
@@ -1,7 +1,9 @@
 class AccountStatementsController < ApplicationController
+  before_action :authenticate_client!
+
   def filter
     initial_date = params[:initial_date].to_date
     end_date = params[:end_date].to_date
-    @statements = AccountStatement.where(move_date: initial_date..end_date)
+    @statements = AccountStatement.where(move_date: initial_date..end_date).where(client: current_client)
   end
 end
--- spec/system/client/client_requests_statement_spec.rb
+++ spec/system/client/client_requests_statement_spec.rb
@@ -1,6 +1,26 @@
 require 'rails_helper'
 
 describe 'client requests statement' do
+  it 'and is not authenticated' do
+    client = create :client
+    stt1 = create(:account_statement, name: 'Déposito', move_date: 3.day.ago, moved_value: 3300, client:)
+    stt2 = create(:account_statement, name: 'Saque', move_date: 2.day.ago, moved_value: -100, client:)
+    stt3 = create(:account_statement, name: 'Saque', move_date: 1.day.ago, moved_value: -500, client:)
+    stt4 = create(:account_statement, name: 'Déposito', move_date: Date.today, moved_value: 1000, client:)
+
+    visit root_path
+    fill_in 'Data Inicial', with: 3.day.ago
+    fill_in 'Data Final', with: 1.day.ago
+    click_on 'Solicitar extrato'
+
+    expect(page).to have_current_path new_client_session_path
+    expect(page).not_to have_content "Déposito | R$ 3.300,00 | id: #{stt1.id} | Data: #{stt1.move_date}"
+    expect(page).not_to have_content "Saque | R$ -100,00 | id: #{stt2.id} | Data: #{stt2.move_date}"
+    expect(page).not_to have_content "Saque | R$ -500,00 | id: #{stt3.id} | Data: #{stt3.move_date}"
+    expect(page).not_to have_content "Déposito | R$ 1.000,00 | id: #{stt4.id} | Data: #{stt4.move_date}"
+    expect(page).not_to have_content 'Nenhuma movimentação encontrada'
+  end
+
   it 'filtered by initial and end date' do
     client = create :client
     stt1 = create(:account_statement, name: 'Déposito', move_date: 3.day.ago, moved_value: 3300, client:)
@@ -18,6 +38,30 @@ describe 'client requests statement' do
     expect(page).to have_content "Saque | R$ -100,00 | id: #{stt2.id} | Data: #{stt2.move_date}"
     expect(page).to have_content "Saque | R$ -500,00 | id: #{stt3.id} | Data: #{stt3.move_date}"
     expect(page).not_to have_content "Déposito | R$ 1.000,00 | id: #{stt4.id} | Data: #{stt4.move_date}"
+  end
+
+  it 'and not sees statements of another clients' do
+    client = create :client
+    client2 = create :client, cpf_number: '09876543212', email: 'test@example.com'
+    stt1 = create(:account_statement, name: 'Déposito', move_date: 3.day.ago, moved_value: 3300, client:)
+    stt2 = create(:account_statement, name: 'Saque', move_date: 2.day.ago, moved_value: -100, client:)
+    stt3 = create(:account_statement, name: 'Saque', move_date: 1.day.ago, moved_value: -500, client:)
+    stt4 = create(:account_statement, name: 'Déposito', move_date: Date.today, moved_value: 1000, client:)
+    stt5 = create(:account_statement, name: 'Saque', move_date: 3.day.ago, moved_value: 2000, client: client2)
+    stt6 = create(:account_statement, name: 'Déposito', move_date: 2.day.ago, moved_value: -600, client: client2)
+
+    login_as client
+    visit root_path
+    fill_in 'Data Inicial', with: 3.day.ago
+    fill_in 'Data Final', with: 1.day.ago
+    click_on 'Solicitar extrato'
+
+    expect(page).to have_content "Déposito | R$ 3.300,00 | id: #{stt1.id} | Data: #{stt1.move_date}"
+    expect(page).to have_content "Saque | R$ -100,00 | id: #{stt2.id} | Data: #{stt2.move_date}"
+    expect(page).to have_content "Saque | R$ -500,00 | id: #{stt3.id} | Data: #{stt3.move_date}"
+    expect(page).not_to have_content "Déposito | R$ 1.000,00 | id: #{stt4.id} | Data: #{stt4.move_date}"
+    expect(page).not_to have_content "Saque | R$ 2.000,00 | id: #{stt5.id} | Data: #{stt5.move_date}"
+    expect(page).not_to have_content "Déposito | R$ 600,00 | id: #{stt6.id} | Data: #{stt6.move_date}"
   end
 
   it 'and theres no results' do
