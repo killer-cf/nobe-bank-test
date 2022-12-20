d58d2eb9667d37d6d744c39b8b1a6cccc67bf240
killer-cf <costa.kilder@live.com>
2022-12-20 14:30:42 -0300

    feat/client sees his balance

--- spec/system/user_visit_home_page_spec.rb
+++ spec/system/user_visit_home_page_spec.rb
@@ -7,4 +7,13 @@ describe 'User visit home page' do
     expect(page).to have_content 'Nobe Bank'
     expect(page).to have_current_path root_path
   end
+
+  it 'and sees his balance' do
+    client = create :client, name: 'Kilder Costa', cpf_number: '12345678901', email: 'ckiler@gmail.com', cash: 1000
+
+    login_as client
+    visit root_path
+
+    expect(page).to have_content 'Saldo: R$ 1.000,00'
+  end
 end
